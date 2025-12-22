78745912701b9fa2c514e9f29c0de0c588799edf
shafeeq <shafeeqap1986@gmail.com>
2025-12-22 21:34:42 +0530

    first commit

---
+++ task-docker/index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Hello from Dockerized Node.js Express Server updated!");
+});
+
+console.log(PORT, 'port');
+
+app.listen(PORT, () => {
+  console.log(`✅ App is running`);
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
